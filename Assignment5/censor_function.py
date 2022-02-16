753d4598088f7986cae787ff52ee1f9ec59349d5
Reid Harry <48355869+Reido50@users.noreply.github.com>
2022-02-16 13:53:53 -0500

    Started Assignment 5

---
+++ Assignment5/censor_function.py
@@ -0,0 +1,9 @@
+# Note not all or your solution should be in the function definition 
+
+def censor(x):
+    censor_list = ["UMich", "Maize", "Blue", "Wolverines"]
+
+
+def add_censor_function(conn):
+  pass
+
