c59ae03765d5f74d4902e0435a5756029da8c202
Reid Harry <48355869+Reido50@users.noreply.github.com>
2022-02-17 13:31:41 -0500

    Finished censor_function

--- Assignment5/censor_function.py
+++ Assignment5/censor_function.py
@@ -1,9 +1,12 @@
 # Note not all or your solution should be in the function definition 
 
 def censor(x):
-    censor_list = ["UMich", "Maize", "Blue", "Wolverines"]
-
+  new_string = x
+  censor_list = ["UMich", "Maize", "Blue", "Wolverines"]
+  for word in censor_list:
+    new_string = new_string.replace(word, "*" * len(word))
+  return new_string
+    
 
 def add_censor_function(conn):
-  pass
-
+  conn.create_function("censor", 1, censor)
